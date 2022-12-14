df88535ec9ecd7cfd2b80094bdf588d81ea8e32c
Jessica <jesscara.993@gmail.com>
2022-12-14 18:47:37 +0100

    moved div.card, connected to api with props info

---
+++ src/components/SingleCharacter.vue
@@ -0,0 +1,46 @@
+<script>
+export default {
+  props: ['info']
+}
+</script>
+
+<template>
+  <div class="card">
+    <img :src="info.image" :alt="info.name">
+    <h4>{{ info.name }}</h4>
+    <h5>{{ info.status }}</h5>
+    <h5>{{ info.species }}</h5>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
+
+.card {
+  background-color: $primary-color;
+  width: calc(100% / 5 - 20px);
+  padding: 10px 10px 40px;
+  margin: 0 10px 20px;
+
+  img {
+    width: 100%;
+  }
+
+  h4 {
+    font-size: 0.7rem;
+    margin: 10px 0;
+    text-transform: uppercase;
+    color: $text-color;
+  }
+
+  h4,
+  h5 {
+    text-align: center;
+  }
+
+  h5 {
+    font-size: 0.7rem;
+    color: $subtext-color;
+  }
+}
+</style>
