f602db077a6d081b539acacc093debe5d1b444f0
Jessica <jesscara.993@gmail.com>
2022-12-14 17:26:24 +0100

    base structure define, no style yet

---
+++ src/components/CharactersList.vue
@@ -0,0 +1,42 @@
+<script>
+import { store } from '../store.js';
+export default {
+  data() {
+    return {
+      store,
+    }
+  }
+}
+</script>
+
+<template>
+  <section class="container">
+    <div class="card">
+      <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="">
+      <h4>nome</h4>
+      <h5>stato</h5>
+      <h5>specie</h5>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
+
+.container {
+  width: 900px;
+  min-height: 400px;
+  margin: 0 auto;
+  background-color: $text-color;
+
+  h4 {
+    text-transform: uppercase;
+  }
+
+  h4,
+  h5 {
+    // da cambiare
+    color: black;
+  }
+}
+</style>
