e3a8a8d05db8eeeb8bf33eeefb6bb4e70951e088
Jessica <jesscara.993@gmail.com>
2022-12-15 18:13:54 +0100

    added select value and parameter needed from api

--- src/store.js
+++ src/store.js
@@ -2,5 +2,7 @@ import { reactive } from 'vue';
 
 export const store = reactive({
   characterList: [],
-  apiURL: 'https://rickandmortyapi.com/api/character',
+  apiURL: 'https://rickandmortyapi.com/api/character/',
+  pickOut: 'status', //value dell'opzione 'Seleziona/Resetta nel select'
+  apiStatusParameter: 'status' //parametro specifico da richiamare dall'api al select dell'user
 });
